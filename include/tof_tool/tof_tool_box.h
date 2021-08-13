0b6f0f003920ae152818dace7b5b330f72091934
Tomos Fearn <tof7@aber.ac.uk>
2021-08-13 21:27:10 +0100

    added rest of cmake and header file

--- include/tof_tool/tof_tool_box.h
+++ include/tof_tool/tof_tool_box.h
@@ -1,9 +1,37 @@
 #ifndef TOF_TOOL_BOX_H
 #define TOF_TOOL_BOX_H
-#include <ros/ros.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ros/ros.h> //main ROS library
+#include <ros/package.h> //find ROS packages, needs roslib dependency
+#include <std_srvs/Empty.h>
+#include "wheelchair_msgs/foundObjects.h"
+#include "wheelchair_msgs/objectLocations.h"
+
+//experimental
+#include "geometry_msgs/PointStamped.h"
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Quaternion.h"
+#include "geometry_msgs/TransformStamped.h"
+
+#include "tf/transform_listener.h"
+#include "tf/transform_broadcaster.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+
 class TofToolBox {
     public:
         void sayHello();
+        void printSeparator(int spaceSize);
+        std::string doesPkgExist(std::string pkg_name);
+        int createFile(std::string fileName);
+
+    private:
+        //nothing here
 };
 
 #endif
