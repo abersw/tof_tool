88640376fe9768429d0b6af1a66f8da3932a7da9
Tomos Fearn <tof7@aber.ac.uk>
2021-08-13 21:18:06 +0100

    added doesPkgExist and printSeparator to library

--- src/tof_tool_box.cpp
+++ src/tof_tool_box.cpp
@@ -1,7 +1,45 @@
 #include "tof_tool/tof_tool_box.h"
 
-        void TofToolBox::sayHello() {
-            ROS_INFO("Hello!");
+const int DEBUG_doesPkgExist = 0;
+
+using namespace std;
+
+void TofToolBox::sayHello() {
+    ROS_INFO("Hello!");
+}
+
+//function for printing space sizes
+// 0  little space
+// 1  big space
+void TofToolBox::printSeparator(int spaceSize) {
+	if (spaceSize == 0) {
+		printf("--------------------------------------------\n");
+	}
+	else {
+		printf("\n");
+		printf("--------------------------------------------\n");
+		printf("\n");
+	}
+}
+
+//does the wheelchair dump package exist in the workspace?
+std::string TofToolBox::doesPkgExist(std::string pkg_name) {
+    std::string getPkgPath;
+	if (ros::package::getPath(pkg_name) == "") {
+        printSeparator(0);
+		cout << "FATAL:  Couldn't find package " << pkg_name << "\n";
+		cout << "FATAL:  Closing node. \n";
+        if (DEBUG_doesPkgExist) {
+            cout << getPkgPath << endl;
         }
-
-
+		ros::shutdown();
+		exit(0);
+	}
+    else {
+        getPkgPath = ros::package::getPath(pkg_name);
+        if (DEBUG_doesPkgExist) {
+            cout << getPkgPath << endl;
+        }
+    }
+    return getPkgPath;
+}
